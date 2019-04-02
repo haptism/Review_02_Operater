dca42e1175fc016679549a63bf861aa33c6d2269
{haptism} <haptism007@gmail.com>
2019-04-02 17:40:13 +0900

    Initial commit

---
+++ src/com/rjm/operator/example/OperatorTest.java
@@ -0,0 +1,10 @@
+package com.rjm.operator.example;
+
+public class OperatorTest {
+	public void sample1(){}
+	public void sample2(){}
+	public void sample3(){}
+	public void sample4(){}
+	public void sample5(){}
+
+}
---
+++ src/com/rjm/operator/example/OperatorTestMain.java
@@ -0,0 +1,10 @@
+package com.rjm.operator.example;
+
+public class OperatorTestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
